a9ede58b4352696b7225defab8bae5347db3b5f5
Demis Bellot <demis.bellot@gmail.com>
2023-01-24 11:36:16 +0800

    Update Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -21,7 +21,7 @@ services.Configure<CookiePolicyOptions>(options =>
 {
     // This lambda determines whether user consent for non-essential cookies is needed for a given request.
     options.CheckConsentNeeded = context => true;
-    options.MinimumSameSitePolicy = SameSiteMode.None;
+    options.MinimumSameSitePolicy = SameSiteMode.Strict;
 });
 
 // To create Identity SQL Server database, change "ConnectionStrings" in appsettings.json
